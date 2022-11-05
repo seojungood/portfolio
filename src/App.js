6b8be27de5000e20874bd48481d36d38e7f8f9e0
seojungood <seojungood@gmail.com>
2022-11-05 02:39:32 -0600

    Added links to navbar and imported to App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import AboutMe from './pages/AboutMe';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/projects' element={<Projects />}/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className='navbar'>
+      <div className='toggleButton'>
+        <button></button>
+      </div>
+      <div className='links'>
+        <Link to='/'> Home </Link>
+        <Link to='/projects'> Projects </Link>
+        <Link to='/aboutme'> About Me </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar
