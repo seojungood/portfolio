ffd1bd493665e955ec272dce00e070af426d57b4
seojungood <seojungood@gmail.com>
2022-11-05 04:10:05 -0600

    Implemented content for homepage like name, links, description, and skills

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GithubIcon from '@material-ui/icons/GitHub';
+import EmailIcon from '@material-ui/icons/Email';
 import '../styles/Footer.css';
 
 function Footer() {
@@ -9,6 +10,7 @@ function Footer() {
       <div className='socialMedia'>
         <LinkedInIcon />
         <GithubIcon />
+        <EmailIcon />
       </div>
       <p> &copy; 2022 Seojun-Chung-Portfolio</p>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,47 @@
 import React from 'react'
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GithubIcon from '@material-ui/icons/GitHub';
+import EmailIcon from '@material-ui/icons/Email';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='about'>
+        <h2>Hello, I'm Seojun Chung</h2>
+        <div className='prompt'>
+          <p> 
+            Software developer with a passion and ambition for learning and creating.
+          </p>
+          <button>Resume</button>
+          <LinkedInIcon />
+          <GithubIcon />
+          <EmailIcon />
+        </div>
+      </div>
+      <div className='skills'>
+        <h1>Skills</h1>
+        <ol className='list'>
+          <li className='item'>
+            <h2>Front-End</h2>
+            <span>
+              ReactJS, HTML, CSS
+            </span>
+          </li>
+          <li className='item'>
+            <h2>Back-End</h2>
+            <span>
+              Firebase, ...Learning MySQL
+            </span>
+          </li>
+          <li className='item'>
+            <h2>Language</h2>
+            <span>
+              Javascript, Java, C#, Typescript
+            </span>
+          </li>
+        </ol>
+      </div>
+    </div>
   )
 }
 
