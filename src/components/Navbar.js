55050297dc1784f76acfdb941ecd8cb9277cb2c1
seojungood <seojungood@gmail.com>
2022-11-05 03:12:27 -0600

    Added functionality of closing navbar upon link clicked

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation} from 'react-router-dom';
 import "../styles/Navbar.css";
 import ReorderIcon from '@material-ui/icons/Reorder';
 
 function Navbar() {
+  // States whether navbar is expanded or not
   const [expandNavbar, setExpandNavbar] = useState(false);
-  
 
+  const location = useLocation();
+
+  //If link is pressed inside navbar, close navbar
+  useEffect(()=>{
+    setExpandNavbar(false);
+  },[location]);
   
   return (
     <div className='navbar' id={expandNavbar ? "open" : "close"}>
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function AboutMe() {
   return (
-    <div>AboutMe</div>
+    <div>About Me</div>
   )
 }
  
