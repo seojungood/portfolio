32c09a2e88b3cfb43e57e4037e643f9a4c138154
seojungood <seojungood@gmail.com>
2022-11-05 03:50:22 -0600

    implemented some css for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GithubIcon from '@material-ui/icons/GitHub';
+import '../styles/Footer.css';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='socialMedia'>
+        <LinkedInIcon />
+        <GithubIcon />
+      </div>
+      <p> &copy; 2022 Seojun-Chung-Portfolio</p>
+    </div>
   )
 }
 
