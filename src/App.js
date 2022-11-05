71c7719e30d9d845fd1dad1a6f093fc1cd4cb1a6
seojungood <seojungood@gmail.com>
2022-11-05 02:34:47 -0600

    Initialized portfolio with home, projects, and about me page

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import AboutMe from './pages/AboutMe';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/projects' element={<Projects />}/>
+          <Route path='/aboutme' element={<AboutMe />}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
