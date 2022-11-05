f8d7e8cee4f6e432b65261a96e740fbde1cdb2c9
seojungood <seojungood@gmail.com>
2022-11-05 03:14:02 -0600

    Imported Footer to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import AboutMe from './pages/AboutMe';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path='/projects' element={<Projects />}/>
           <Route path='/aboutme' element={<AboutMe />}/>
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
