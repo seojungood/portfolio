f7b1bb35efae5b859dbfe66d58b72c7f0f54ac1e
seojungood <seojungood@gmail.com>
2024-01-25 04:12:22 -0700

    added social media project

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,23 +1,23 @@
-import Spreadsheet from '../assets/Spreadsheet.jpg';
-import pixelDrawer from '../assets/pixelDrawer.jpeg';
-import GameDay from '../assets/GameDay.jpg';
-import Todolist from '../assets/todolist.webp';
-import simonGame from '../assets/simonGameImage.jpeg';
-import DOF from '../assets/depthOfPerceptionimg.png';
-import FFR from '../assets/ffrimg.png';
-import firestrike_img from '../assets/vr_img.jpeg'
-import icegunga_img from '../assets/icegunga_img.png'
+import Spreadsheet from "../assets/Spreadsheet.jpg";
+import pixelDrawer from "../assets/pixelDrawer.jpeg";
+import GameDay from "../assets/GameDay.jpg";
+import Todolist from "../assets/todolist.webp";
+import simonGame from "../assets/simonGameImage.jpeg";
+import DOF from "../assets/depthOfPerceptionimg.png";
+import FFR from "../assets/ffrimg.png";
+import firestrike_img from "../assets/vr_img.jpeg";
+import icegunga_img from "../assets/icegunga_img.png";
+import socialmedia_img from "../assets/socialmedia.jpeg";
 
-import pixelDrawerDoc from '../documents/pixelDrawerDescription.pdf'
-import SpreadsheetREADME from '../documents/SpreadsheetREADME.pdf';
-import GameDayWhitePaper from '../documents/GameDayWhitePaper.pdf';
-import GameDayInfographic from '../documents/GameDayInfographic.pdf';
-import simonGameREADME from '../documents/SimoneGameREADME.pdf';
-import DOFREADME from '../documents/unityDoFREADME.pdf';
-import FFRREADME from '../documents/UnityFFRREADME.pdf';
-import firestrikePPT from '../documents/firestrike_ppt.pdf'
-import icegungeREADME from '../documents/IceGungaMasterREADME.pdf'
-
+import pixelDrawerDoc from "../documents/pixelDrawerDescription.pdf";
+import SpreadsheetREADME from "../documents/SpreadsheetREADME.pdf";
+import GameDayWhitePaper from "../documents/GameDayWhitePaper.pdf";
+import GameDayInfographic from "../documents/GameDayInfographic.pdf";
+import simonGameREADME from "../documents/SimoneGameREADME.pdf";
+import DOFREADME from "../documents/unityDoFREADME.pdf";
+import FFRREADME from "../documents/UnityFFRREADME.pdf";
+import firestrikePPT from "../documents/firestrike_ppt.pdf";
+import icegungeREADME from "../documents/IceGungaMasterREADME.pdf";
 
 export const ProjectList = [
   {
@@ -25,62 +25,71 @@ export const ProjectList = [
     image: pixelDrawer,
     skills: "Qt Creator, C++",
     githubLink: "https://gitfront.io/r/seojungood/7d5LC6SfnN65/pixel-drawer/",
-    demoLink: "https://youtu.be/o6MAB7HMz7c"
+    demoLink: "https://youtu.be/o6MAB7HMz7c",
   },
   {
     name: "Spreadsheet",
     image: Spreadsheet,
     skills: "C#, MAUI, XAML",
     githubLink: "https://gitfront.io/r/seojungood/aNVx1mgykFWj/spreadsheet/",
-    demoLink: "https://youtu.be/qKMoC4Z3LO0"
+    demoLink: "https://youtu.be/qKMoC4Z3LO0",
   },
   {
     name: "Unity - Depth of Perception",
     image: DOF,
     skills: "C#, Unity",
-    githubLink: "https://gitfront.io/r/seojungood/7UwQWBQzPkKN/assignment-2-transformations-and-depth-perception-seojungood/",
-    demoLink: "https://youtu.be/-NoYFAVE6hQ"
+    githubLink:
+      "https://gitfront.io/r/seojungood/7UwQWBQzPkKN/assignment-2-transformations-and-depth-perception-seojungood/",
+    demoLink: "https://youtu.be/-NoYFAVE6hQ",
   },
   {
     name: "VR - FireStrike",
     image: firestrike_img,
     skills: "C#, Unity",
     githubLink: "https://github.com/seojungood/VR-Firestrike",
-    demoLink: "https://youtu.be/dsrShgEOhGo"
+    demoLink: "https://youtu.be/dsrShgEOhGo",
   },
   {
     name: "Qt Creator - Icecream Making Tutorial",
     image: icegunga_img,
     skills: "Qt Creator, C++, Box2D",
     githubLink: "https://github.com/seojungood/Icecream-make-tutorial",
-    demoLink: "https://youtu.be/ymb5l3xP8Z4"
+    demoLink: "https://youtu.be/ymb5l3xP8Z4",
   },
   {
     name: "Unity - Fixed Foveated Rendering / Bloom Effect",
     image: FFR,
     skills: "C#, Shader, Unity",
-    githubLink: "https://gitfront.io/r/tjwnsgood/UAAmvwMyKoYN/assignment-3-foveated-rendering-seojungood/",
-    demoLink: "https://youtu.be/C26XB8kTuMw"
+    githubLink:
+      "https://gitfront.io/r/tjwnsgood/UAAmvwMyKoYN/assignment-3-foveated-rendering-seojungood/",
+    demoLink: "https://youtu.be/C26XB8kTuMw",
   },
   {
     name: "Super Bowl Twitter Data Analysis",
     image: GameDay,
     skills: "Python, Tableau, Tableau Prep",
     githubLink: GameDayWhitePaper,
-    demoLink: GameDayInfographic
+    demoLink: GameDayInfographic,
+  },
+  {
+    name: "Social Media App (Learning Cloud)",
+    image: socialmedia_img,
+    skills: "Typescript, React, HTML/CSS, Firebase(Cloud)",
+    githubLink: "https://github.com/seojungood/Social-Media-Project",
+    demoLink: "https://youtube.com/shorts/cqZWYH93yJg",
   },
   {
     name: "Simon Game",
     image: simonGame,
     skills: "Qt Creator, C++",
     githubLink: simonGameREADME,
-    demoLink: "https://youtu.be/xqcWQGgMkao"
+    demoLink: "https://youtu.be/xqcWQGgMkao",
   },
   {
     name: "Todo List (Next.js)",
     image: Todolist,
     skills: "Next.js, TypeScript, Prisma, Tailwind css",
     githubLink: "https://github.com/seojungood/TodoList-nextjs",
-    demoLink: "https://youtu.be/Ulvz1D29GDA"
-  }
-]
+    demoLink: "https://youtu.be/Ulvz1D29GDA",
+  },
+];
